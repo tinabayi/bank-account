dcc965a636c34dde233a5a64daccd872e9d6019e
tinabayi <bayizerechristine4@gmail.com>
2019-01-19 11:58:27 +0200

    Putting background image

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,48 @@
-function Bank(name,initialdeposit,accountnumber,depositamount,withdrawamount){
+function Contact(name,idNumber,initialDeposit){
     this.name=name;
-    this.initialdeposit=initiadeposit;
-    this.accountnumber=accountnumber;
-    this.depositamount=depositamount;
-    this.withdrawamount=withdrawamount;
-
+    this.idNumber=idNumber;
+    this.initialDeposit=initialDeposit;
 }
+$(document).ready(function(event) {
+   
+    $("form#new-user").submit(function(event) {
+      
+    event.preventDefault();
+      var inputtedName = $("input#new-name").val();
+      var inputtedId = $("input#id-number").val();
+      var inputtedInitialDeposit = $("input#initial-deposit").val();
+      
+  
+      var newContact = new Contact(inputtedName, inputtedId,inputtedInitialDeposit);
+  
+      $("#contacts").text(newContact.initialDeposit);
+  
+      $("input#new-name").val("");
+      $("input#id-number").val("");
+      $("input#initial-deposit").val("");
 
+    });
 
-$(document).ready(function() {
-    $("form#blanks").submit(function(event) {
-        event.preventDefault
+    $("form#deposit-withdraw").submit(function(event) {
+        event.preventDefault();
+   
+      var inputtedDepositAmount = $("input#deposit").val();
+        var inputtedWithdrawAmount = $("input#withdraw").val();
+     
+        Contact.prototype.deposit=function(){
+            var amountDeposit= this.initialDeposit + inputtedDepositAmount;
+           $("#contacts").text(amountDeposit);
+        }
+        Contact.prototype.withdraw=function(){
+             return inputtedWithdrawAmount ;
+           $("#deposit-withdraw").text(inputtedWithdrawAmount);
 
-        $(".clickable").hide();
-        $(".current").show();
-        var initiaDeposit = $("input#deposit").val();
-$(".story").text(initiaDeposit);
-    })
-
-
+           $("input#deposit").val("");
+           $("input#withdraw").val("");
+           
+     
+  
+  }); 
 });
+   
+    
