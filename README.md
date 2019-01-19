8a146a7e3ef491fadbf16fe9fb83d498c3e976e6
tinabayi <bayizerechristine4@gmail.com>
2019-01-19 13:20:20 +0200

    Idone

---
+++ README.md
@@ -0,0 +1,25 @@
+# Project name
+~~~
+Bank account
+~~~
+### Author
+~~~
+BAYIZERE CHRISTINE
+~~~
+### Description
+~~~
+Is a web page where a user can create account and  deposit amount of 
+money and it will see the balace automatically,even if user deposit or withdraw will alsose his balance.
+~~~
+### Technology used
+~~~~
+HTML
+CSS
+JS
+Jquery
+Bootstrap
+~~~~
+## Licence information
+~~~
+This project is licensed under the MIT License - see the LICENSE.md file for details
+~~~
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
+//Business logic
 function Contact(name,idNumber,initialDeposit){
     this.name=name;
     this.idNumber=idNumber;
     this.initialDeposit=initialDeposit;
 }
+var inputtedDepositAmount = 0;
+
 $(document).ready(function(event) {
    
-    $("form#new-user").submit(function(event) {
+    $("form#deposit").submit(function(event) {
       
     event.preventDefault();
-      var inputtedName = $("input#new-name").val();
+      var inputtedDepositAmount = $("input#deposit-amount").val();
+      console.log
       var inputtedId = $("input#id-number").val();
       var inputtedInitialDeposit = $("input#initial-deposit").val();
       
   
       var newContact = new Contact(inputtedName, inputtedId,inputtedInitialDeposit);
+      
   
       $("#contacts").text(newContact.initialDeposit);
   
@@ -22,28 +27,47 @@ $(document).ready(function(event) {
       $("input#initial-deposit").val("");
 
     });
-
-    $("form#deposit-withdraw").submit(function(event) {
+//user interface logic
+    $("form#new-user").submit(function(event) {
+      
         event.preventDefault();
-   
-      var inputtedDepositAmount = $("input#deposit").val();
-        var inputtedWithdrawAmount = $("input#withdraw").val();
-     
-        Contact.prototype.deposit=function(){
-            var amountDeposit= this.initialDeposit + inputtedDepositAmount;
-           $("#contacts").text(amountDeposit);
-        }
-        Contact.prototype.withdraw=function(){
-             var balance= amountDeposit-inputtedWithdrawAmount ;
-        }
-           $("#deposit-withdraw").text(balance);
+          var inputtedName = $("input#new-name").val();
+          var inputtedId = $("input#id-number").val();
+          var inputtedInitialDeposit = $("input#initial-deposit").val();
+          
+      
+          var newContact = new Contact(inputtedName, inputtedId,inputtedInitialDeposit);
+      
+          $("#contacts").text(newContact.initialDeposit);
+      
+          $("input#new-name").val("");
+          $("input#id-number").val("");
+          $("input#initial-deposit").val("");
+    });
+});  
 
-           $("input#deposit").val("");
-           $("input#withdraw").val("");
+//      $("form#deposit").submit(function(event) {
+//         event.preventDefault();
+   
+//       var inputtedDepositAmount = $("input#deposit-amount").val();
+//        console.log(inputtedDepositAmount)
+//          var inputtedWithdrawAmount = $("input#withdraw").val();
+     
+//          Contact.prototype.deposit=function(){
+//              var amountDeposit= this.initialDeposit + inputtedDepositAmount;
+//              console.log(amountDeposit)
+//             $("#contacts").text(amountDeposit);
+//          }
+//         Contact.prototype.withdraw=function(){
+//               var balance= amountDeposit-inputtedWithdrawAmount ;
+//         }            $("#deposit-withdraw").text(balance);
+
+//            $("input#deposit").val("");
+//            $("input#withdraw").val("");
            
      
   
-  }); 
-});
+//    }); 
+// });
    
     
